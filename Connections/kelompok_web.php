5f2d671c4447ceaf0585ca8c8b0492165656ed34
sherlockholmesjr <52303227+sherlockholmesjr@users.noreply.github.com>
2019-06-28 10:44:38 +0700

    Create kelompok_web.php

---
+++ Connections/kelompok_web.php
@@ -0,0 +1,7 @@
+<?php 
+$dbname = "kelompok_web";
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpasw = "";
+
+$link = mysqli_connect($dbhost, $dbuser, $dbpasw, $dbname) or die(mysqli_connect_error());
