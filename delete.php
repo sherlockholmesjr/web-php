b5cc6c3a1ca7fef13165a18fcc213b314f89f29f
sherlockholmesjr <52303227+sherlockholmesjr@users.noreply.github.com>
2019-06-28 08:30:32 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+require_once "../../Connections/kelompok_web.php";
+$id = mysqli_real_escape_string($link, $_GET['id']);
+$sql = "DELETE FROM admin WHERE id = ?";
+if($st = mysqli_prepare($link, $sql)){
+    mysqli_stmt_bind_param($st, 's', $id);
+    if(mysqli_stmt_execute($st)){
+        header("Location: daftar.php? hapus sukses");
+        exit();
+    }
+}
